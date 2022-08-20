ac3aaba0c4a480b54f5b34c951a0ee013e7d8a49
IVainqueur <ishimvainqueur@gmail.com>
2022-08-20 16:25:31 +0200

    refactoring-db30151a18cf4373a04df8622f52e5e9

--- js/issue.js
+++ js/issue.js
@@ -122,7 +122,7 @@ async function checkForBook(){
 }
 async function checkForUser(){
     document.querySelector('#StudentCodeOrNameBox').className = "checking"
-    let what = parseInt(document.querySelector('#StudentCodeOrNameBoxInput').value) === NaN ? "code" : "names"
+    let what = parseInt(document.querySelector('#StudentCodeOrNameBoxInput').value)  ? "code" : "names"
     await fetch("/checkForUser", {
         method: "POST",
         headers: {
